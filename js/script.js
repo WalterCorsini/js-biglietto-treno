36c71ae1cbf694525b1412e444c93cfebffcc136
WalterCorsini <corsini.walter@gmail.com>
2024-03-25 17:45:42 +0100

    fix some bugs

--- js/script.js
+++ js/script.js
@@ -22,34 +22,32 @@ console.log(userAge, typeof userAge);
 let price = userKm * 0.21; // number
 result = price;
 
-
-
 // check age or km isNaN else apply discoutn by age
 if(isNaN(userAge) || isNaN(userKm)){
     result = "Hai inserito i dati in modo non corretto";
-} else if(userAge < 18 && userAge <=65){   
+} else if(userAge < 18){   
     price = price * 0.8;
 
     // fixed 2 number after ,
     price = price.toFixed(2);
-    result = `il costo del tuo biglietto per percorrere ${userKm}km in base ai tuoi anni è di ${price}€`; 
-} else{
+    result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price}€`; 
+} else if(userAge > 65){
     price = price * 0.6;
-    
+
     // fixed 2 number after ,
     price = price.toFixed(2);
-    result = `il costo del tuo biglietto per percorrere ${userKm}km in base ai tuoi anni è di ${price}€`;
+    result = `il costo del tuo biglietto per percorrere${userKm} km in base ai tuoi anni è di ${price}€`; 
 }
-
+else{
+    result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price}€`; 
+    }
 
 // OUTPUT
 
+
+
 // print to video result
 document.getElementById("result").innerHTML = result;
-
-
-
-
 
 
 
