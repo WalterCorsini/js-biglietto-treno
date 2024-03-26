ce94113f4cf802f7ce5349aaa50c52f5356a113a
WalterCorsini <corsini.walter@gmail.com>
2024-03-26 17:53:01 +0100

    add: logo

--- js/script.js
+++ js/script.js
@@ -1,53 +1,3 @@
-// // test
-// // alert("ciao");
-
-// // INPUT
-
-// // variable result
-// let result;
-
-// // ask km
-// let userKm = prompt("Quanti kilometri devi percorrere?"); //string | null
-// userKm = parseInt(userKm); // string to number
-// console.log(userKm, typeof userKm);
-
-// // ask age
-// let userAge = prompt("Quanti anni hai?"); //string | null
-// userAge = parseInt(userAge); // string to number
-// console.log(userAge, typeof userAge);
-
-// //LOGIC
-
-// // calculate price for km
-// let price = userKm * 0.21; // number
-// result = price;
-
-// // check age or km isNaN else apply discoutn by age
-// if(isNaN(userAge) || isNaN(userKm)){
-//     result = "Hai inserito i dati in modo non corretto";
-// } else if(userAge < 18){   
-//     price = price * 0.8;
-
-//     // fixed 2 number after ,
-//     result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
-// } else if(userAge > 65){
-//     price = price * 0.6;
-
-//     // fixed 2 number after ,
-//     result = `il costo del tuo biglietto per percorrere${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
-// }
-// else{
-//     result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
-// }
-
-// // OUTPUT
-
-// // print to video result
-// document.getElementById("result").innerHTML = result;
-
-// test
-// alert("ciao");
-
 // 1. find button on page
 const sendBtnElem = document.getElementById("send-btn"); // object | null
 
@@ -100,8 +50,6 @@ sendBtnElem.addEventListener("click", function(){
         - applicando lo sconto, il prezzo del biglietto è  <strong>€${priceWithDiscount.toFixed(2)}</strong>.
     </span>
     `
-
-    // document.getElementById("result").innerHTML = `il costo del tuo biglietto di base è ${priceWithoutDiscount}, sulla base della tua età abbiamo visto che hai uno scont del ${discount} %, quindi il tuo biglietto subirà uno sconto di €${priceOfTheDiscount.toFixed(2)}, e dovrai pagare un totale di €${priceWithDiscount.toFixed(2)}`;
     
 })
 
