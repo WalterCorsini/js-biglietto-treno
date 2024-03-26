2c8d4667b5c8531dfa623cca12a0d4d29e23d85c
WalterCorsini <corsini.walter@gmail.com>
2024-03-26 16:56:44 +0100

    fix: refactoring README e js file

--- README.md
+++ README.md
@@ -1,26 +1,38 @@
 #CALCOLARE IL PREZZO DEL BIGLIETTO DELL'UTENTE IN BASE AI KILOMETRI ED ALLA SUA ETA'
-1. Chiediamo all'utente quanti chilometri deve percorrere.
-2. Chiediamo all'utente quandi anni ha.
-3. Calcoliamo il prezzo da pagare.
-4. Applichiamo uno sconto in base all'età.
-5. Stampiamo a video il prezzo finale con non piu di 2 decimali.
 
-1. ##INPUT
-    - [x] Dichiaro una variabile per salvare il prezzo del      
-            biglietto.
-    - [x] Chiediamo all'utente quanti chilometri deve percorrere.
-    - [x] Chiediamo all'utente quanti anni ha.
 
-2. ##LOGICA
-    - [x] Calcoliamo il prezzo in base ai chilometri da 
-            percorrere.
-    - [x] Controllo se l'età o i km sono scritti in modo non 
-            corretto(NULL, testo, spazio vuoto)anziche numeri.
-    - [x] altrimenti calcolo lo sconto in base all'età.
-    - [x] Trasformiamo il prezzo finale  in modo che non abbia  
-            piu di 2 cifre decimali.
-    - [x] Salvo il risultato nella variabile dichiarata prima  
-            nell'input.
+1. L'utente inserisce quanti chilometri deve percorrere.
+2. l'utente inserisce quanti anni ha.
+3. faccio premere invio all'utente per inviare i 
+        dati.
+4. Calcolo il prezzo di base del biglietto.
+5. Controllo che l'utente non abbia fatto errori di 
+        compilazione.
+6. Applichiamo uno sconto in base all'età.
+7. Stampiamo a video il prezzo finale con non piu di 
+        2 decimali.
 
-3. ##OUTPUT
-    - [x] Stampiamo a video il costo del biglietto.
+## 1. INPUT
+- [ ] Dichiaro una variabile per lo sconto
+- [ ] Dichiaro una variabile per salvare il prezzo 
+        del biglietto.
+- [ ] Chiediamo all'utente quanti chilometri deve 
+        percorrere.
+- [ ] Chiediamo all'utente quanti anni ha.
+
+
+## 2. LOGICA
+- [ ] Calcoliamo il prezzo in base ai chilometri da 
+        percorrere.
+- [ ] Controllo se l'età o i km sono scritti in modo 
+        non corretto(NULL, testo, spazio vuoto
+        numeri negativi).
+- [ ] se tutto è corretto calcolo lo sconto in base 
+        all'età.
+- [ ] Trasformiamo il prezzo finale  in modo che non 
+        abbia piu di 2 cifre decimali.
+- [ ] Salvo il risultato nella variabile dichiarata 
+        prima nell'input.
+
+## 3. OUTPUT
+- [ ] Stampiamo a video il costo del biglietto.
--- js/script.js
+++ js/script.js
@@ -1,51 +1,97 @@
+// // test
+// // alert("ciao");
+
+// // INPUT
+
+// // variable result
+// let result;
+
+// // ask km
+// let userKm = prompt("Quanti kilometri devi percorrere?"); //string | null
+// userKm = parseInt(userKm); // string to number
+// console.log(userKm, typeof userKm);
+
+// // ask age
+// let userAge = prompt("Quanti anni hai?"); //string | null
+// userAge = parseInt(userAge); // string to number
+// console.log(userAge, typeof userAge);
+
+// //LOGIC
+
+// // calculate price for km
+// let price = userKm * 0.21; // number
+// result = price;
+
+// // check age or km isNaN else apply discoutn by age
+// if(isNaN(userAge) || isNaN(userKm)){
+//     result = "Hai inserito i dati in modo non corretto";
+// } else if(userAge < 18){   
+//     price = price * 0.8;
+
+//     // fixed 2 number after ,
+//     result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
+// } else if(userAge > 65){
+//     price = price * 0.6;
+
+//     // fixed 2 number after ,
+//     result = `il costo del tuo biglietto per percorrere${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
+// }
+// else{
+//     result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
+// }
+
+// // OUTPUT
+
+// // print to video result
+// document.getElementById("result").innerHTML = result;
+
 // test
 // alert("ciao");
 
+// 1. find button on page
+const sendBtnElem = document.getElementById("send-btn"); // object | null
+
+let userKm;
+let userAge;
+let discount = 0;
+let priceWithoutDiscount = 0;
+let priceOfTheDiscount = 0;
+let priceWithDiscount =0;
+
 // INPUT
 
-// variable result
-let result;
+// 2. add eventListener at button
+sendBtnElem.addEventListener("click", function(){
+    let userKm = parseInt(document.getElementById("user-km").value);
+    console.log(userKm, typeof userKm);
+    let userAge = parseInt(document.getElementById("user-age").value);
+    console.log(userAge, typeof userAge);
 
-// ask km
-let userKm = prompt("Quanti kilometri devi percorrere?"); //string | null
-userKm = parseInt(userKm); // string to number
-console.log(userKm, typeof userKm);
+// LOGIC
 
-// ask age
-let userAge = prompt("Quanti anni hai?"); //string | null
-userAge = parseInt(userAge); // string to number
-console.log(userAge, typeof userAge);
+    // calculate discount
+    if(userAge < 18){
+        discount = 20;
+    } else if(userAge > 65){
+        discount = 40;
+    } else {
+        discount = 0;
+    }
+    // calculate price Without Discount
+    priceWithoutDiscount = userKm * 0.21;
 
-//LOGIC
-
-// calculate price for km
-let price = userKm * 0.21; // number
-result = price;
-
-// check age or km isNaN else apply discoutn by age
-if(isNaN(userAge) || isNaN(userKm)){
-    result = "Hai inserito i dati in modo non corretto";
-} else if(userAge < 18){   
-    price = price * 0.8;
-
-    // fixed 2 number after ,
-    result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
-} else if(userAge > 65){
-    price = price * 0.6;
-
-    // fixed 2 number after ,
-    result = `il costo del tuo biglietto per percorrere${userKm} km in base ai tuoi anni è di ${price.toFixed(2)}€`; 
-}
-else{
-    result = `il costo del tuo biglietto per percorrere ${userKm} km in base ai tuoi anni è di ${price}€`; 
-}
+    // calculate price of the discount
+    priceOfTheDiscount = priceWithoutDiscount * discount / 100;
+    
+    // calculate price with the discount
+    priceWithDiscount = priceWithoutDiscount - priceOfTheDiscount;
 
 // OUTPUT
 
-// print to video result
-document.getElementById("result").innerHTML = result;
-
-
+    // print to video
+    document.getElementById("result").innerHTML = `il costo del tuo biglietto di base è ${priceWithoutDiscount}, sulla base della tua età abbiamo visto che hai uno scont del ${discount} %, quindi il tuo biglietto subirà uno sconto di €${priceOfTheDiscount.toFixed(2)}, e dovrai pagare un totale di €${priceWithDiscount.toFixed(2)} `;
+    
+})
 
 
 
